35b9c1230b06fdbc7dda3421dedbe67ba8765d0c
Neo Östlund Zetterberg <neo.ostlundzetterbe@learnet.se>
2025-11-21 17:50:18 +0100

    Fixed a bug with the achivements.

--- achievements.py
+++ achievements.py
@@ -9,7 +9,7 @@ class Achievement:
     @property
     def text(self) -> str:
         lines = self.description.split("\n")
-        return f"{self.name} - {"\n".join(list(line if i == 0 else " "*(len(self.name)+3) for i, line in enumerate(lines)))}"
+        return f"{self.name} - {"\n".join(list(line if i == 0 else " "*(len(self.name)+3)+line for i, line in enumerate(lines)))}"
 
 ACHIEVEMENTS = {
     "First Game": Achievement("First Game", "Everything starts at your first game."),
