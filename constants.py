ef7a6da83fa71d80a69b20240b269b532b4b73f4
Neo Östlund Zetterberg <neo.ostlundzetterbe@learnet.se>
2025-11-22 12:37:52 +0100

    A whole bunch of new names and informational loading text.

--- constants.py
+++ constants.py
@@ -8,7 +8,18 @@ class LANGUAGE:
 NAMES = [
     "Alisa",
     "Faur",
-    "Likare"
+    "Likare",
+    "Goblin",
+    "Dragon Of Askarin",
+    "Smuglin",
+    "Cooper",
+    "Nightmare",
+    "The Thieves of The Graveyard",
+    "Tamil",
+    "Karel",
+    "Tora",
+    "Tarim",
+    "Ant Atom"
 ]
 
 RARITY_COLOR = {
@@ -28,8 +39,21 @@ RARITY_REWARD = {
 }
 
 LOADING_TEXT = [
-    "Did you know that you cant lose the reavers pike?"
-    
+    "Did you know that you cant lose the reavers pike?",
+    "You can sell bad items to the trader and recieve half the price.",
+    "Green is common, blue is uncommon, magenta is rare and red are legendary.",
+    "Buy the reavers pike, and never lose it.",
+    "One achievement requires you to use the trident",
+    "Get five items and you will get an achievement",
+    "There is a secret game modes, only the developers know of.",
+    "The escalibur is very powerful, deals 20 damage, but is very unlikely to hit.",
+    "The reavers pike is half as common as the escalibur.",
+    "The axe has a 95%% chance to hit.",
+    "You can always retreat if you play on easy mode.",
+    "The costs and earnings after every battle is carefully calculated based on how powerful the item is.",
+    "Even if you lose, you are lucky to get 5 loses, which gives you an award!",
+    "Win five times to get an award!",
+    "The name of every enemy is generated randomly."
 ]
 
 RANDOM_SEED = None
